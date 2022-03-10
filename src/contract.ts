72aa1e6261a3b58e0a0c2d1dcf46a4f574fcc8bf
terramarkets <admin@terra.markets>
2022-03-10 09:47:53 +0100

    add coins to executeBet

--- src/contract.ts
+++ src/contract.ts
@@ -1,5 +1,6 @@
 import { LCDClient, MsgExecuteContract } from '@terra-money/terra.js';
 import { AccAddress } from '@terra-money/terra.js/dist/core/bech32';
+import { Coins } from "@terra-money/terra.js/dist/core/Coins";
 
 export enum BetDirection {
   Up = 'up',
@@ -85,7 +86,8 @@ export interface RoundResponse {
 }
 
 export class TerraMarketsContract {
-  constructor(public contractAddress: AccAddress) {}
+  constructor(public contractAddress: AccAddress) {
+  }
 
   fabricateCloseMarket() {
     return { close_market: {} };
@@ -204,8 +206,8 @@ export class TerraMarketsContract {
     return new MsgExecuteContract(accAddress, this.contractAddress, this.fabricateUpdateConfig(owner, tax, min_bet));
   }
 
-  executeBet(accAddress: string, round_id: number, amount: string, direction: BetDirection): MsgExecuteContract {
-    return new MsgExecuteContract(accAddress, this.contractAddress, this.fabricateBet(round_id, amount, direction));
+  executeBet(accAddress: string, round_id: number, amount: string, direction: BetDirection, coins: Coins.Input): MsgExecuteContract {
+    return new MsgExecuteContract(accAddress, this.contractAddress, this.fabricateBet(round_id, amount, direction), coins);
   }
 
   executeClaim(accAddress: string, rounds: number[]): MsgExecuteContract {
